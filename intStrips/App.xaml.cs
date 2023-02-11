04fabe18e05e866748b3936193e69a3881e7b9a1
Chelsea Pritchard <chelsea@dotefekts.net>
2023-02-11 11:38:55 +0800

    Initial commit.

---
+++ intStrips/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace intStrips
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ intStrips/Controls/FlightStripControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using intStrips.Models;
+using System;
+using System.Windows.Controls;
+
+namespace intStrips.Controls
+{
+    public partial class FlightStripControl : UserControl
+    {
+        public FlightStripControl()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
